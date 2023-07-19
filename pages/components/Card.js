4f8b915a2369280fa92bf0edfdd8060fab0b2efd
Anny dos Santos Sena Vaz <annysena.as44@gmail.com>
2023-07-19 19:06:41 -0300

    component Card

---
+++ pages/components/Card.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "../../styles/Card.module.css"
+
+export default function Card({ pokemon }) {
+    const getImage = (id) => {
+        let str = "" + id;
+		let pad = "000";
+		return pad.substring(0, pad.length - str.length) + str;
+    }
+    return (
+        <div className={styles.card}>
+            <img
+            src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${getImage(pokemon.id)}.png`}
+            width="120"
+            height="120"
+            alt={pokemon.name}
+            />
+            <p className={styles.id}>#{pokemon.id}</p>
+            <h3 className={styles.title}>{pokemon.name}</h3>
+            <Link href={`/pokemon.id`}>  
+              <span className={styles.btn}>Detalhes</span>
+            </Link>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Card from './components/Card'
+
 export async function getStaticProps() {
 
   const maxPokemons = 251
@@ -20,13 +24,23 @@ export async function getStaticProps() {
 
 export default function Home({ pokemons }) {
   return (
-    <div>
-      <h1>PokeNext</h1>
-      <ul>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image 
+        src="/images/pokeball.png"
+        width="50"
+        height="50"
+        alt="PokeNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name}</li>
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   )
 }
