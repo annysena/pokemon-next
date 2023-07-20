b1f0ce8efb982d3dd77f01bd77d749709fbc079c
Anny dos Santos Sena Vaz <annysena.as44@gmail.com>
2023-07-20 13:28:31 -0300

    iniciando página individual de Pokemons

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../../styles/Card.module.css"
+import styles from "../styles/Card.module.css"
 
 export default function Card({ pokemon }) {
     const getImage = (id) => {
